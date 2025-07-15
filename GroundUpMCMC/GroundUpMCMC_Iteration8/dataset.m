f2f1bf665e4f2a6f7ee7e48f3a730f6cf75b074b
Noah McLean <noahmc@ku.edu>
2025-07-15 17:09:46 -0400

    Scott tasks for dataset class constructor

--- GroundUpMCMC/GroundUpMCMC_Iteration8/dataset.m
+++ GroundUpMCMC/GroundUpMCMC_Iteration8/dataset.m
@@ -3,16 +3,34 @@ classdef dataset
     %   Includes constructor and vectorizing methods
     
     properties
-        Property1
+        intensity       (:,1) double  = []
+        time            (:,1) double  = []
+        integrationTime (:,1) double  = []
+        detectorIndex   (:,1) uint8   = []
+        sequenceIndex   (:,1) uint8   = []
+        mass            (:,1) double  = []
+        isotope         (:,1) uint8   = []
+        block           (:,1) uint16  = []
+        cycle           (:,1) uint32  = []
+        isOP            (:,1) logical = []
+        isUsed          (:,1) logical = []
     end
     
     methods
-        function obj = dataset(inputArg1,inputArg2)
-            %DATASET Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+        function obj = dataset(varargin)
+            
+        if nargin > 0 % if constructred with inputs
+            data = cell2struct(varargin(1));
+            method = cell2struct(varargin(2));
+            % Scott: replicate assembleDataVector(data, method) here
+            % to create a dataset
+
+            
+
+        end % if constructed from a data input
+
         end
-        
+       
         function outputArg = method1(obj,inputArg)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
